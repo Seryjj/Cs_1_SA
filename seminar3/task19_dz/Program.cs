da78eae559109b778af6ce0c732c3321c44b188b
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-09-11 14:38:38 +0300

    Выполнение task19 версия 3

--- seminar3/task19_dz/Program.cs
+++ seminar3/task19_dz/Program.cs
@@ -27,19 +27,33 @@
 // }
 
 
-﻿//разворот числа
-int number = 12345; //исходное число
-int tempNumber = number;//число, которое мы будем делить на 10
+
+ Console.WriteLine("Введите пятизначное число :");
+ int number = Convert.ToInt32(Console.ReadLine());
+ int tempNumber = number;//число, которое мы будем делить на 10
 int reversedNumber = 0; // развернутое число
+
+
+ if (number>=10000 && number<=99999)
+{
+
+
 while(tempNumber > 0)
 {
     int digit = tempNumber % 10;
     reversedNumber = reversedNumber * 10 + digit;
     tempNumber = tempNumber / 10;
 }
-if (reversedNumber=tempNumber)
+
+
+if (number==reversedNumber)
 Console.WriteLine("Да");
 else Console.WriteLine("Нет");
 
+}
+else
+{
+Console.WriteLine("Введено не пятизначное число");
 
+}
 
