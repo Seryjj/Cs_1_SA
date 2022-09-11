3ed761cfb2029995c8b2048d602808597b29ac5d
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-09-11 23:53:21 +0300

    Выполнение task29

---
+++ seminar4/task29_dz/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+
+
+int[] array= new int[8];
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i}-е число:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
