aa601fe08d1ce36c68b1ff17b65c8ff451d1da7e
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-09-17 13:38:39 +0300

    Выполнение task38

---
+++ seminar5/task38_dz/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+
+int[]array = {3,7,22,2,78};
+
+
+Console.WriteLine(string.Join(", ", array));
+
+int max=array[0];
+   for(int i = 0; i < array.Length; i++)
+    {
+        if (max<array[i])
+        {
+            max=array[i];
+        }
+
+    }
+
+int min=array[0];
+   for(int i = 0; i < array.Length; i++)
+    {
+        if (min>array[i])
+        {
+            min=array[i];
+        }
+
+    }
+
+
+    Console.WriteLine(max-min);
