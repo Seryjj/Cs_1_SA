5b9795d79e9d65aa45c84fc0b55786bf57c0c2ba
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-09-22 01:31:30 +0300

    Выполнение task42

---
+++ seminar6/task42/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintBinaryView(int number)
+{
+    if(number <= 0) 
+    {
+        return;
+    }
+    PrintBinaryView(number / 2);
+    Console.Write(number % 2);
+}
+
+int num = ReadNumber("Введите число:");
+PrintBinaryView(num);
