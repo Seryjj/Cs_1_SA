99d56165cc5951e287fb3d2ad2231f4601a1ca9c
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-10-02 19:38:13 +0300

    Выполнение task58

---
+++ seminar8/task58_dz/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// Например, заданы 2 массива:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// и
+
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+
+// Их произведение будет равно следующему массиву:
+
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+
+
+
+
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr1 = GetMatrix(m, n);
+int[,] matr2 = GetMatrix(m, n);
+int[,] matr3= new int[m, n];
+PrintMatrix(matr1);
+Console.WriteLine();
+PrintMatrix(matr2);
+Console.WriteLine();
+    for(int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr1.GetLength(1); j++)
+        {
+Console.Write( matr1[i,j]*matr2[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
