f7114e62c2e63835164255211c1367d34fcff46e
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-10-02 19:38:47 +0300

    Выполнение task60

---
+++ seminar8/task60_dz/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу,
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+// массив размером 2 x 2 x 2
+
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+
+
+
+
+
+
+void FillArrayRandomNumber(int[,,] array, int minNumber = 0, int maxNumber = 9)
+{
+ Random rand = new Random();
+
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+ for (int j = 0; j < array.GetLength(1); j++)
+        {
+ for (int k = 0; k < array.GetLength(2); k++)
+            {
+ while (array[i, j, k] == 0)
+                {
+ int number = rand.Next(minNumber, maxNumber + 1);
+
+                    if (IsNumberInArray(array, number) == false)
+                    {
+                        array[i, j, k] = number;
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+bool IsNumberInArray(int[,,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == number) return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+void PrintArrayWithIdex(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k]);
+                Console.Write("({0},{1},{2})\t", i, j, k);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+
+
+int arraySizeX = 2;
+int arraySizeY = 2;
+int arraySizeZ = 2;
+int minNumber = 10;
+int maxNumber = 99;
+int[,,] testArray = new int[arraySizeX, arraySizeY, arraySizeZ];
+
+FillArrayRandomNumber(testArray, minNumber, maxNumber);
+PrintArrayWithIdex(testArray);
