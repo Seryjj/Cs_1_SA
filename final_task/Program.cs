af458756c206edc19d54b3c4b2e32373b260f362
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-10-22 21:27:00 +0300

    Написание функций расчёта

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void MassivMensche3(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintMassiv(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
